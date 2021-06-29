e1f4604b1623f4d1a9b09e616c4021db1e3dfe69
Subham Karmakar <subhamk.subs@gmail.com>
2021-06-30 01:18:27 +0530

    Edited the Brandname spacing

--- src/components/landing/first-landing-section/BrandName.tsx
+++ src/components/landing/first-landing-section/BrandName.tsx
@@ -5,7 +5,7 @@ export default function BrandName() {
   return (
     <div className="brand_container py-4 px-4">
       <h1>
-        <span className="name_1">#include&lt;</span>
+        <span className="name_1">#include &lt;</span>
         <span className="name_2">TYPE</span>
         <span className="name_1">&gt;</span>
       </h1>
