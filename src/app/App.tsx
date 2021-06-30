c17c296fb66042045801d175e2ead96c9c837f47
Subham Karmakar <subhamk.subs@gmail.com>
2021-06-30 23:22:42 +0530

    Added the Dev Security Page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { ReactElement, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-// import LoginPage from "../components/login/LoginPage";
 import LandingPage from "../components/landing/LandingPage";
+import DevCheck from "../dev-check/DevCheck";
 
-function App() {
+function App(): ReactElement {
+  const password: string = "rmssd";
+  const [userInput, setUserInput] = useState<string>("");
+
   return (
     <Router>
       <div className="App">
-        <LandingPage />
-        {/* <LoginPage/> */}
+        {(userInput === "" || userInput !== password) ? (
+          <DevCheck
+            password={password}
+            userInput={userInput}
+            setUserInput={setUserInput}
+          />
+        ) : (
+          <LandingPage />
+        )}
       </div>
     </Router>
   );
---
+++ src/dev-check/DevCheck.tsx
@@ -0,0 +1,41 @@
+import { ReactElement } from "react";
+import "./DevCheck.css";
+
+interface DevCheckProps {
+    password: string,
+    userInput: string,
+    setUserInput: Function
+};
+
+function DevCheck(props: DevCheckProps): ReactElement {
+    return (
+        <div className="Dev">
+            <header className="Dev-header">
+                <p>This application is under development!</p>
+                <p>
+                    If you're a member of the development team, then please
+                    enter the password to proceed...
+                </p>
+                <br />
+                <input
+                    type="password"
+                    className="Dev-input"
+                    placeholder="Password"
+                    value={props.userInput}
+                    onInput={e => props.setUserInput(e.currentTarget.value)}
+                />
+                {props.userInput === "" ? (
+                    <p></p>
+                ) : (
+                    props.userInput !== props.password ? (
+                        <p className="Input-info-red">❌ Incorrect Password!</p>
+                    ) : (
+                        <p></p>
+                    )
+                )}
+            </header>
+        </div>
+    );
+}
+
+export default DevCheck;
