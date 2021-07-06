e18c5e47d413d7eaf8c23593cde6d4458a65d698
Subham Karmakar <subhamk.subs@gmail.com>
2021-07-06 20:35:10 +0530

    Completed the Log In Mechanisms

--- src/components/landing/LandingPage.tsx
+++ src/components/landing/LandingPage.tsx
@@ -1,19 +1,90 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
+import React, { ReactElement, useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Introduction from "./introduction/Introduction";
 import LoginPage from "../login/LoginPage";
 import SignUp from "../signup/SignUp";
+import { User } from "../../models/User";
+import Dashboard from "../dashboard/Dashboard";
+import { LoadingSpinnerLarge } from "../spinners/Spinners";
 
-export default function LandingPage() {
+export default function LandingPage(): ReactElement {
+  const [user, setUser] = useState<User>({
+    userId: "",
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: ""
+  });
+  const [loginComplete, setLoginComplete] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function getAuthenticatedUser(): Promise<void> {
+      try {
+        const response = await fetch("https://include-type.herokuapp.com/api/user/authenticateduser/", {
+          credentials: "include"
+        });
+        if (response.ok) {
+          // console.log("Authenticated User Received");
+          const jsonUser: User = await response.json();
+          // console.log(jsonUser);
+          setUser(jsonUser);
+          setLoginComplete(true);
+        }
+        else {
+          throw new Error();
+        }
+      }
+      catch (error) {
+        setUser(prevUser => ({
+          ...prevUser,
+          userId: "-1"
+        }));
+        // console.log("Invalid Token or Currently Not Logged In!");
+      }
+    }
+
+    getAuthenticatedUser();
+  }, [loginComplete]);
+
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/" exact component={Introduction} />
-          <Route path="/LoginPage" component={LoginPage} />
-          <Route path="/SignUpPage" component={SignUp} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      {user.userId === "-1" ? (
+        <Router>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              component={Introduction}
+            />
+            <Route
+              path="/LoginPage"
+              exact
+              component={
+                () => <LoginPage
+                  setUser={setUser}
+                  setLoginComplete={setLoginComplete}
+                />
+              }
+            />
+            <Route
+              path="/SignUpPage"
+              exact
+              component={SignUp}
+            />
+          </Switch>
+        </Router>
+      ) : (
+        user.userId === "" ? (
+          <LoadingSpinnerLarge />
+        ) : (
+          <Dashboard
+            user={user}
+            setUser={setUser}
+            setLoginComplete={setLoginComplete}
+          />
+        )
+      )}
+    </div>
   );
 }
