e11af9c7a630dbddcd722b875c3eccfaee6bde42
J-Caesar2000 <monosijnayek@gmail.com>
2021-07-06 13:09:32 +0530

    bullshit commit

--- src/components/application/Application.tsx
+++ src/components/application/Application.tsx
@@ -4,11 +4,11 @@ import "./application.css";
 export default function Application() {
   return (
     <section id="Application_page">
-      <section id="Status"></section>
+      {/* <section id="Status"></section>
       <section id="Main_area">
         <section id="Application_menu_area"></section>
         <section id="Application_content_area"></section>
-      </section>
+      </section> */}
     </section>
   );
 }
