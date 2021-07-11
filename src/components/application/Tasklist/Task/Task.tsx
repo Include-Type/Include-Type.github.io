4e1b640556b75c92042fa49409c8d3b682919438
Srijita <srijitachakrabarty007@gmail.com>
2021-07-11 15:37:35 +0530

    tasklist 1.1

--- src/components/application/Tasklist/Task/Task.tsx
+++ src/components/application/Tasklist/Task/Task.tsx
@@ -1,41 +1,67 @@
 import React, { useState } from 'react';
+import { ReactElement } from 'react';
 import "./task.css";
 import { Task } from './TaskInterface';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 interface TaskProps {
     key: string,
     data: Task
   }
 
-export default function EachTask(props: TaskProps) {
-    function drop(){
-        setTaskLiskShow(!taskListShow);
-    }
+export default function EachTask(props: TaskProps): ReactElement {
     
     const [taskListShow,setTaskLiskShow] = useState<boolean>(false);
+    const [fullHeading,showFullHeading] = useState<string>("task_title");
+    const [striked,setStriked] = useState<string>("task_content");
 
+    function drop(): void{
+        setTaskLiskShow(!taskListShow);
+        if(fullHeading === "task_title")
+        {
+            showFullHeading("show_full_task_title");
+        }else{
+            showFullHeading("task_title");
+        }
+    }
+    
+
+    function strike(): void{
+        if(striked === "task_data")
+        {
+            setStriked("task_data striked");
+        }else{
+            setStriked("task_data");
+        }
+    }
     return (
         <div className="task">
             <div className="task_heading" onClick={drop}>
                 <div className="task_deadline">Due on: {props.data.deadline}</div>
                 <span className="bar"></span>
                 <div className="task_proj_name"><b>{props.data.proj_name}</b></div>
-                <div className="task_title">{props.data.title}</div>
+                <div className={fullHeading}>{props.data.title}</div>
                 <div className="task_date">{props.data.date}</div>
             </div>
             {taskListShow ? (
                 <div className="task_content">
-                    <div className="task_assigned">Assigned to: <span className="assigned_name">{props.data.assigned}</span></div>
-                    <div className="task_data">{props.data.details}</div>
+                    <div>
+                        <div className="task_assigned">Assigned to: <span className="assigned_name">{props.data.assigned}</span></div>
+                        <div className={striked}>{props.data.details}</div>
+                    </div>
+                    <div className="functions">
+                        <div className="far fa-trash-alt">Delete</div>
+                        <div onClick={strike} className="far fa-check-square">Read</div>
+                        <div className="far fa-bell">Important!</div>
+                    </div>
                 </div>
             ) : (
                 <p></p>
             )}
-            <div className="functions">
-                <i className="far fa-check-square"></i>
-                <i className="far fa-bell"></i>
-                <i className="far fa-trash-alt"></i>
-            </div>
+                    {/* <FontAwesomeIcon icon={['far','bell']}/> */}
+                    {/* <FontAwesomeIcon icon="fa-regular fa-user"/> */}
+            
         </div>
     )
 }
