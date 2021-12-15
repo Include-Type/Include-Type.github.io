2abf1a8ef673a0476dd51f22844409c8488bf832
J-Caesar2000 <monosijnayek@gmail.com>
2021-12-15 19:22:24 +0530

    sidemenubar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,53 +1,76 @@
-import React, {useState} from "react";
-import { RiMenuLine, RiLayoutGridFill, RiChat4Fill, RiTeamFill, RiTaskFill, RiPieChart2Fill} from "react-icons/ri";
+import React, { useState } from "react";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded";
+import ChatBubbleRoundedIcon from "@mui/icons-material/ChatBubbleRounded";
+import GroupsRoundedIcon from "@mui/icons-material/GroupsRounded";
+import AssignmentRoundedIcon from "@mui/icons-material/AssignmentRounded";
+import InsightsRoundedIcon from "@mui/icons-material/InsightsRounded";
 import "./sidebar.css";
-
-
+import SidebarTabs from "./SidebarTabs";
+import { Tab } from "./Tab";
 
 function Sidebar() {
-    const sidebarCollapsed = localStorage.getItem("sidebar-collapsed");
-    const [isExpanded, setIsExpanded] = useState(sidebarCollapsed ? false : true);
-    const handleToggler = () => {
-        if (isExpanded) {
-            setIsExpanded(false);
-            localStorage.setItem('sidebar-collapsed',"true");
-            return;
-        }
-        setIsExpanded(true);
-        localStorage.removeItem('sidebar-collapsed');
-    };
-    return(
-        
-        <div className={isExpanded ? "Sidebar" : "Sidebar collapsed"}>
-            <div className="sidebar-header">
-                <RiMenuLine className = "sidebar-icon"
-                 onClick={handleToggler}
-                />
-                <h1 className="sidebar-logo">Include-Type</h1>
-            </div>
-            <div className="sidebar-items">
-                <div className="item">
-                  <RiLayoutGridFill className = "sidebar-icon"/>
-                  <span className="sidebar-text">Dashboard</span>
-                </div>
-                <div className="item">
-                  <RiChat4Fill className = "sidebar-icon"/>
-                  <span className="sidebar-text">Chat</span>
-                </div>
-                <div className="item">
-                  <RiTeamFill className = "sidebar-icon"/>
-                  <span className="sidebar-text">Teams</span>
-                </div>
-                <div className="item">
-                  <RiTaskFill className = "sidebar-icon"/>
-                  <span className="sidebar-text">Task</span>
-                </div>
-                <div className="item">
-                  <RiPieChart2Fill className = "sidebar-icon"/>
-                  <span className="sidebar-text">Analytics</span>
-                </div>
-            </div>
-        </div>
-    );
+  const tabs: Tab[] = [
+    {
+      icon_component: DashboardRoundedIcon,
+      name: "Dashboard",
+    },
+    {
+      icon_component: ChatBubbleRoundedIcon,
+      name: "Chat",
+    },
+    {
+      icon_component: GroupsRoundedIcon,
+      name: "Teams",
+    },
+    {
+      icon_component: AssignmentRoundedIcon,
+      name: "Tasks",
+    },
+    {
+      icon_component: InsightsRoundedIcon,
+      name: "Analytics",
+    },
+  ];
+
+  const [isExpanded, setIsExpanded] = useState(true);
+  const handleToggler = () => {
+    if (isExpanded) {
+      setIsExpanded(false);
+      return;
+    }
+    setIsExpanded(true);
+  };
+  return (
+    <div className={isExpanded === true ? "sidebar" : "collapsed_sidebar"}>
+      <div className="sidebar-header">
+        {isExpanded === true ? (
+          <ArrowBackIcon
+            sx={{ fontSize: 35 }}
+            onClick={handleToggler}
+            className="sidebar-menutoggler-arrow"
+          />
+        ) : (
+          <MenuRoundedIcon
+            sx={{ fontSize: 35 }}
+            onClick={handleToggler}
+            className="sidebar-menutoggler-ham"
+          />
+        )}
+
+        {isExpanded === true ? (
+          <div className="sidebar-brand">Include-Type</div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+      <div className="sidebar-items">
+        {tabs.map((tab: Tab) => (
+          <SidebarTabs key={tab.name} sidetab={tab} isExpanded={isExpanded} />
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Sidebar;
