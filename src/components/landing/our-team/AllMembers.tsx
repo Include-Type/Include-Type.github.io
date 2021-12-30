a57d292731bbbe66f9a20b4a8d97dd60413cf30e
Monosij Nayek <monosijnayek@gmail.com>
2021-12-30 21:26:10 +0530

    our team images

--- src/components/landing/our-team/AllMembers.tsx
+++ src/components/landing/our-team/AllMembers.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import my_image from "../../../Resources/Images/our_team_images/dummy.png";
+import debayan from "../../../Resources/Images/our_team_images/Debayan.png";
+import monosij from "../../../Resources/Images/our_team_images/Monosij.png";
+import rishab from "../../../Resources/Images/our_team_images/Rishab.png";
+import rohan from "../../../Resources/Images/our_team_images/Rohan.png";
+import srijita from "../../../Resources/Images/our_team_images/Srijita.png";
+import subham from "../../../Resources/Images/our_team_images/Subham.png";
+
 import "../css/our-team-css/AllMembers.css";
 import TeamMember from "./TeamMember";
 import { Member } from "./Member";
@@ -10,37 +17,37 @@ export default function AllMembers() {
       name: "Monosij Nayek",
       role: "Front-end Developer",
       email: "monosijnayek@gmail.com",
-      picture: my_image,
+      picture: monosij,
     },
     {
       name: "Srijita Chakrabarty",
       role: "Front-end Developer",
       email: "srijitachakrabarty007@gmail.com",
-      picture: my_image,
+      picture: srijita,
     },
     {
       name: "Rishab Sengupta",
       role: "Front-end Developer",
       email: "rishabsengupta09062000@gmail.com",
-      picture: my_image,
+      picture: rishab,
     },
     {
       name: "Rohan Halder",
       role: "Front-end Designer",
       email: "rohanhalder13@gmail.com",
-      picture: my_image,
+      picture: rohan,
     },
     {
       name: "Subham Karmakar",
       role: "Front-end & Back-end Developer",
       email: "subhamkarmakar090@gmail.com",
-      picture: my_image,
+      picture: subham,
     },
     {
       name: "Debayan De",
       role: "Back-end Developer",
       email: "debayan.pog@gmail.com",
-      picture: my_image,
+      picture: debayan,
     },
   ];
 
