0cd6e6f7c444a3f5e684ee693a3b9d546a6ddf56
Monosij Nayek <monosijnayek@gmail.com>
2021-12-31 12:50:58 +0530

    admin save buttons & snackbars

--- src/components/application/Admin/Admin Pages/CreateAssignTasks.tsx
+++ src/components/application/Admin/Admin Pages/CreateAssignTasks.tsx
@@ -25,16 +25,12 @@ const MenuProps = {
 };
 
 const names = [
-  "Oliver Hansen",
-  "Van Henry",
-  "April Tucker",
-  "Ralph Hubbard",
-  "Omar Alexander",
-  "Carlos Abbott",
-  "Miriam Wagner",
-  "Bradley Wilkerson",
-  "Virginia Andrews",
-  "Kelly Snyder",
+  "Monosij Nayek",
+  "Subham Karmakar",
+  "Rohan Halder",
+  "Rishab Sengupta",
+  "Srijita Chakrabarty",
+  "Debayan De",
 ];
 
 export default function CreateAssignTasks() {
--- src/components/application/Admin/NewUpdateProject.tsx
+++ src/components/application/Admin/NewUpdateProject.tsx
@@ -8,6 +8,19 @@ import NavigateNextRoundedIcon from "@mui/icons-material/NavigateNextRounded";
 import NavigateBeforeRoundedIcon from "@mui/icons-material/NavigateBeforeRounded";
 import SaveIcon from "@mui/icons-material/Save";
 
+import Snackbar, { SnackbarCloseReason } from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps, AlertColor } from "@mui/material/Alert";
+// import { User } from "../../../../models/User";
+// import { LoadingSpinnerMedium } from "../../../spinners/Spinners";
+import { CircularProgress } from "@mui/material";
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 const adminNavigationButtonStyle: CSSProperties = {
   width: "7vw",
   height: "4.5vh",
@@ -34,7 +47,28 @@ const saveButtonStyle: CSSProperties = {
 
 export default function NewUpdateProject() {
   const [Page, setPage] = useState<number>(1);
+  const [status, setStatus] = useState<string>("stopped");
+  const [open, setOpen] = useState<boolean>(false);
+  const [updateInfo, setUpdateInfo] = useState<string>("");
+  const [updateResult, setUpdateResult] = useState<AlertColor | undefined>(
+    undefined
+  );
+  
+  const handleClick = () => {
+    setOpen(true);
+  };
 
+  const handleClose = (
+    event: Event | React.SyntheticEvent<Element, Event>,
+    reason?: string | SnackbarCloseReason
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+  
   const NextPageToggler = () => {
     if (Page < 3) {
       setPage(Page + 1);
@@ -57,7 +91,38 @@ export default function NewUpdateProject() {
         {Page === 1 ? (
           <div className="title_page_container">
             <TitlePage />
-            <div className="d-flex justify-content-end px-2 mt-3">
+            <div className="d-flex justify-content-between px-2 mt-3">
+            <Button
+                disabled={status === "started" ? true : false}
+                // onClick={(e) => updateProfile(e)}
+                type="submit"
+                variant="contained"
+                color="primary"
+                size="medium"
+                // className={classes.saveButton}
+                style={saveButtonStyle}
+                startIcon={status === "started" ? "" : <SaveIcon />}
+              >
+                {status === "started" ? (
+                  <CircularProgress size={26} style={{ color: "white" }} />
+                ) : (
+                  "Save"
+                )}
+              </Button>
+              <Snackbar
+                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                open={open}
+                autoHideDuration={3000}
+                onClose={handleClose}
+              >
+                <Alert
+                  onClose={handleClose}
+                  severity={updateResult}
+                  style={{ fontSize: 18 }}
+                >
+                  {updateInfo}
+                </Alert>
+              </Snackbar>
               <Button
                 style={adminNavigationButtonStyle}
                 variant="contained"
@@ -80,6 +145,37 @@ export default function NewUpdateProject() {
               >
                 Prev
               </Button>
+              <Button
+                disabled={status === "started" ? true : false}
+                // onClick={(e) => updateProfile(e)}
+                type="submit"
+                variant="contained"
+                color="primary"
+                size="medium"
+                // className={classes.saveButton}
+                style={saveButtonStyle}
+                startIcon={status === "started" ? "" : <SaveIcon />}
+              >
+                {status === "started" ? (
+                  <CircularProgress size={26} style={{ color: "white" }} />
+                ) : (
+                  "Save"
+                )}
+              </Button>
+              <Snackbar
+                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                open={open}
+                autoHideDuration={3000}
+                onClose={handleClose}
+              >
+                <Alert
+                  onClose={handleClose}
+                  severity={updateResult}
+                  style={{ fontSize: 18 }}
+                >
+                  {updateInfo}
+                </Alert>
+              </Snackbar>
               <Button
                 style={adminNavigationButtonStyle}
                 variant="contained"
@@ -112,6 +208,20 @@ export default function NewUpdateProject() {
                 startIcon={<SaveIcon />}
               >Save
               </Button>
+              <Snackbar
+                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                open={open}
+                autoHideDuration={3000}
+                onClose={handleClose}
+              >
+                <Alert
+                  onClose={handleClose}
+                  severity={updateResult}
+                  style={{ fontSize: 18 }}
+                >
+                  {updateInfo}
+                </Alert>
+              </Snackbar>
             </div>
           </div>
         )}
